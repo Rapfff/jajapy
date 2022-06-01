31e0dcfef599dca59f997c42dad22e242c1913db
kraken_university <raphal20@ru.is>
2022-06-01 12:31:40 +0000

    setup updated

--- setup.py
+++ setup.py
@@ -1,22 +1,34 @@
 import pathlib
-from setuptools import setup
-
+from setuptools import setup, find_packages
+from codecs import open
 
 HERE = pathlib.Path(__file__).parent
 
 # The text of the README file
-README = (HERE / "README.md").read_text()
-
+with open(HERE / "README.md", encoding='utf-8') as f:
+	long_description = f.read()
 setup(
     name='jajapy',
-    long_description=README,
+    long_description=long_description,
     packages=['jajapy'],
     install_requires=['numpy', 'scipy'],
     long_description_content_type="text/markdown",
-    version='0.0.1',
+    version='0.1',
     url="",
-    description='''''',
+    description='Baum-Welch for all kind of Markov model',
     author='Raphaël Reynouard',
     author_email="raphal20@ru.is",
     license='MIT',
+	classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent"
+    ],
+    include_package_data=True
 )
