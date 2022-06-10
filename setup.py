cf254aa46389c860a7d17160f878b2a99e3f74ff
kraken_university <raphal20@ru.is>
2022-06-10 16:05:16 +0000

    update setup for new release

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     packages=['jajapy'],
     install_requires=['numpy', 'scipy'],
     long_description_content_type="text/markdown",
-    version='0.2',
+    version='0.3',
     url="",
     description='Baum-Welch for all kind of Markov model',
     author='Raphaël Reynouard',
